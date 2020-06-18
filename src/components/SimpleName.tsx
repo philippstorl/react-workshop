13c63890eb102ffb5027c4f39e7e3cf084ea44ad
Philipp Storl <philipp@staffbase.com>
2020-06-18 14:48:07 +0200

    Initial commit

---
+++ src/components/SimpleName.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const name: string = 'Philipp';
+
+const SimpleName: React.FunctionComponent = () => {
+    return(<div>Hello {name}</div>);
+}
+
+export default SimpleName;
